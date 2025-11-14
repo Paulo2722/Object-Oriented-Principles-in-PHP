a7d545d5f927506ad27af58158957877c6f8d820
Paulo <pescanellasotero@gmail.com>
2025-11-14 10:24:51 +0100

    Episodio 01 - Breve introducción acerca del funcionamiento de las clases y las funciones y como deben ser estrucutaras e identificadas

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+class Post{
+    
+    public function archive(){
+
+    }
+
+    public function share(){
+
+    }
+}
+
+class Coment{
+
+    public function respond(){
+
+    }
+}
+
+class Photo{
+
+    public function convert(){
+
+    }
+
+    public function upscalse(){
+
+    }
+}
