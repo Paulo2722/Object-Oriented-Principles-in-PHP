f0a42470da12c16bd8d920118c4d142f9a359579
Paulo <pescanellasotero@gmail.com>
2025-11-14 11:23:02 +0100

    Episodio 02 - Creo una clase PlayList con una función que me permite barajar las canciones de una array y otra función que me permite construir

--- index.php
+++ index.php
@@ -1,5 +1,29 @@
 <?php
 
+class PlayList{
+
+    public function __construct(public $name, public $songs){
+    }
+
+    public function shuffle(){
+        shuffle($this->songs);
+    }
+}
+
+$playlists = [];
+
+$playlists[] = new PlayList('80s Headbangers', [
+    'Back in Black',
+    'Hells Bells',
+    'Highway to Hell'
+]);
+
+die(var_dump($playList));
+
+
+
+
+/*
 class Post{
     
     public function archive(){
@@ -28,3 +52,4 @@ class Photo{
 
     }
 }
+*/
